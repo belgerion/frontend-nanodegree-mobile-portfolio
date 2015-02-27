a12ee77b64392a9481ca7f331480bed08b6c4b17
belgerion <belgerion@gmail.com>
2015-02-27 10:39:33 -0700

    jsupdate

--- views/js/main.js
+++ views/js/main.js
@@ -503,10 +503,10 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var phase = document.body.scrollTop / 1250;
+  var move = document.body.scrollTop / 1250;
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((phase) + (i % 5));
+    var phase = Math.sin((move) + (i % 5));
     items[i].style.transform = 'translate3d(' + (100 * phase) + 'px,0,0)';
   }
 
