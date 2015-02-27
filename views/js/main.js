4e80dd9da2bd2af0c3000540401d5db82d6d28dd
belgerion <belgerion@gmail.com>
2015-02-27 10:34:58 -0700

    js update

--- views/js/main.js
+++ views/js/main.js
@@ -507,7 +507,7 @@ function updatePositions() {
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((phase) + (i % 5));
-    items[i].style.transform = items[i].basicLeft + 100 * phase + 'px';
+    items[i].style.transform = translate + (100 * phase) + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
