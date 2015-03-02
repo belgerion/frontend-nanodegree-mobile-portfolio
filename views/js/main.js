de8a56498f7a9c030dcdaf6626eb1db49a1bdc57
belgerion <belgerion@gmail.com>
2015-03-02 14:09:29 -0700

    pizza update

--- views/js/main.js
+++ views/js/main.js
@@ -504,8 +504,6 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var move = document.body.scrollTop / 1250;
-  var items = document.querySelectorAll('.mover');
-  var number = items.length;
   for (var i = 0; i < number; i++) {
     var phase = Math.sin((move) + (i % 5));
     items[i].style.transform = 'translate3d(' + (100 * phase) + 'px,0,0)';
@@ -538,5 +536,8 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
+  var items = document.querySelectorAll('.mover');
+  var number = items.length;
+
   updatePositions();
 });
